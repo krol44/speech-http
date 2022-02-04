28ea0bebb9aa28a349e3a9de5589e97000a9b1c1
krol44 <krol44@me.com>
2022-02-05 01:01:18 +0300

    fix

--- docker-start.sh
+++ docker-start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker build . -t speech-http-c
 docker rm -f speech-http
-docker run -d --restart=always -p 8048:8048 --log-opt max-size=50m --name=speech-http speech-http-c
+docker run -d --restart=always -p 127.0.0.1:8048:8048 --log-opt max-size=50m --name=speech-http speech-http-c
 
 docker image prune -f
